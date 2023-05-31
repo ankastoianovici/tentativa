6f888199b72bb967d8e4050fc7aee20e73905f8b
Anka-Snezana Stoianovici <anka.stoianovici@gmail.com>
2023-05-31 22:10:00 +0300

    31.05

--- app/src/main/java/com/example/licenta/develop.java
+++ app/src/main/java/com/example/licenta/develop.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -22,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,17 +31,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class develop extends Fragment {
+public class develop extends Fragment implements View.OnTouchListener {
 
-    Button button,openButton;
+    Button button, openButton;
     private ImageView selectedImageTextView;
-
+    TextView textView;
+    float xCoordonate, yCoordonate;
+    private float startX;
+    private float startY;
 
     //Problema e ca nu-mi place deloc cum arata meniul cu linii
-    int images[]={R.drawable.dottedline,R.drawable.dottedline2,R.drawable.dottedline3,
-            R.drawable.dottedline4,R.drawable.dottedline5,R.drawable.dottedline6,
-            R.drawable.line,R.drawable.line2,R.drawable.line3,
-            R.drawable.line4,R.drawable.line5,R.drawable.line6,};
+    int images[] = {R.drawable.dottedline, R.drawable.dottedline2, R.drawable.dottedline3,
+            R.drawable.dottedline4, R.drawable.dottedline5, R.drawable.dottedline6,
+            R.drawable.line, R.drawable.line2, R.drawable.line3,
+            R.drawable.line4, R.drawable.line5, R.drawable.line6,};
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -54,8 +59,11 @@ public class develop extends Fragment {
         animationDrawable.start();
 
         button = rootView.findViewById(R.id.add);
-        openButton=rootView.findViewById(R.id.openButton);
+        openButton = rootView.findViewById(R.id.openButton);
         selectedImageTextView = rootView.findViewById(R.id.selectedImageTextView);
+        textView = rootView.findViewById(R.id.textselectedImageTextView);
+        selectedImageTextView.setOnTouchListener(this);
+        textView.setOnTouchListener(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +80,7 @@ public class develop extends Fragment {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 View view = LayoutInflater.from(getContext()).inflate(R.layout.legenda, null);
                 GridView gridView = view.findViewById(R.id.gridView2);
-                CustomAdapter adapter=new CustomAdapter(getActivity(), images);
+                CustomAdapter adapter = new CustomAdapter(getActivity(), images);
                 gridView.setAdapter(adapter);
                 builder.setView(view);
                 AlertDialog dialog = builder.create();
@@ -82,6 +90,8 @@ public class develop extends Fragment {
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         int selectedImage = images[position];
                         selectedImageTextView.setImageResource(Integer.parseInt(String.valueOf(selectedImage)));
+                        //valoarea din spatele imaginii
+                        textView.setText(String.valueOf(selectedImage));
                         dialog.dismiss();
 
                     }
@@ -117,8 +127,44 @@ public class develop extends Fragment {
             }
         });
 
-
-
         return rootView;
     }
+//-----------------------------------------------------------------------------------------------------
+    //Miscare image si text
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // Record the starting coordinates of the touch
+                startX = event.getX();
+                startY = event.getY();
+                return true;
+
+            case MotionEvent.ACTION_MOVE:
+                // Calculate the distance moved by the touch
+                float dx = event.getX() - startX;
+                float dy = event.getY() - startY;
+
+                // Get the current position of the views
+                int imageViewX = (int) selectedImageTextView.getX();
+                int imageViewY = (int) selectedImageTextView.getY();
+                int textViewX = (int) textView.getX();
+                int textViewY = (int) textView.getY();
+
+                // Move both views by adding the distance moved to their respective current positions
+                selectedImageTextView.setX(imageViewX + dx);
+                selectedImageTextView.setY(imageViewY + dy);
+                textView.setX(textViewX + dx);
+                textView.setY(textViewY + dy);
+
+                // Update the starting coordinates for the next move
+                startX = event.getX();
+                startY = event.getY();
+                return true;
+
+            default:
+                return false;
+        }
+    }
 }
+
